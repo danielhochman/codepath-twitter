787324eaee1f7cc660c1d77049bc6aa8b41c932f
Daniel Hochman <dhochman@lyft.com>
2015-03-15 13:32:39 -0700

    static fragment

---
+++ app/src/main/java/com/codepath/apps/twitter/fragments/TweetsListFragment.java
@@ -0,0 +1,24 @@
+package com.codepath.apps.twitter.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.apps.twitter.R;
+
+public class TweetsListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tweets_list, container, false);
+        return v;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
